c65b019ccb131b0c6606a1c765b642964341c9b2
emre gemici <gmcc.emree@gmail.com>
2021-12-23 18:29:23 +0300

    Add project files.

---
+++ MCD_DLL/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_DLL
+{
+    public class Class1
+    {
+        public int Toplama(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public int Cikarma(int sayi1, int sayi2)
+        {
+            return sayi1 - sayi2;
+        }
+
+        public int Carpma(int sayi1, int sayi2)
+        {
+            return sayi1 * sayi2;
+        }
+
+        public int Bolme(int sayi1, int sayi2)
+        {
+            if (sayi2 == 0)
+            {
+                sayi2 = 1;
+            }
+            return sayi1 / sayi2;
+        }
+    }
+}
---
+++ MCD_DLL_Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_DLL_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MCD_DLL.Class1 MatematikselIslemler = new MCD_DLL.Class1();
+            int sonuc = MatematikselIslemler.Toplama(80, 25);
+            Console.WriteLine(sonuc);
+
+            Console.ReadKey();
+
+        }
+    }
+}
